ebd115fdb21ae668d8f2e4ca6e0c6b0c5c0a1865
Korolev <kdpvt2b@gmail.com>
2023-12-27 11:44:17 +0500

    Рефакторинг стилей

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/init.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/header.php";
 $oCashMachine = new CashMachine();
 $arCashMachines = $oCashMachine->getCashMachine();
-$iCounter = 1;
+$iColor = 1;
 $oUserAuthorized = false;
 ?>
-    <section class="cashmachine">
+    <section class="cashmachine-wrapper" hidden>
         <div class="container">
             <?php if(!empty($arCashMachines)): ?>
                 <div class="cashmachine__inner">
-                    <?php foreach($arCashMachines as $iKeyCashMachine => $arCashMachine):
-                        if($iCounter > $iKeyCashMachine) $iCounter = 1;
-                        ?>
-                        <ul class="cashmachine__list">
+                    <ul class="cashmachine__list">
+                        <?php foreach($arCashMachines as $iKeyCashMachine => $arCashMachine):
+                            if($iColor > 3) $iColor = 1;
+                            ?>
                             <li class="cashmachine__item js-cashmachine-item" data-color="green">
-                                <figure><img src="/local/images/atm-machines-<?=$iCounter?>.jpg" alt=""></figure>
+                                <figure><img src="/local/images/atm-machines-<?=$iColor?>.jpg" alt=""></figure>
                                 <div class="cashmachine__item--inner">
                                     <div class="cashmachine__item--group">
                                         <div class="cashmachine__item--group-name">Адрес:</div>
@@ -31,14 +31,14 @@ $oUserAuthorized = false;
                                     </div>
                                 </div>
                             </li>
-                        </ul>
-                        <?php $iCounter++;
-                    endforeach; ?>
+                            <?php $iColor++;
+                        endforeach; ?>
+                    </ul>
                     <div class="cashmachine__select js-cashmachine-select">
                         <div class="cashmachine__select--wrapp red js-cashmachine-wrap">
                             <div class="cashmachine__select--top">
                                 <div class="cashmachine__select--terminal">
-                                    <?php if(!$oUserAuthorized):?>
+                                    <?php if(!$oUserAuthorized): ?>
                                         <form action="" class="authorized" method="post">
                                             <div class="form-group">
                                                 <input type="text" id="name" name=FIELDS[PIN_CODE] autocomplete="off" required readonly placeholder="Пин код ****">
@@ -60,6 +60,7 @@ $oUserAuthorized = false;
                                     </div>
                                     <div class="cashmachine__select--cheque">
                                         <figure><img src="local/images/cheque.png" alt=""></figure>
+                                        <?php /*
                                         <div class="cashmachine__select--cheque--paper">
                                             <div class="cashmachine__select--cheque--paper-text js-cashmachine-select-cheque-paper-text">
                                                 <p></p>
@@ -69,6 +70,7 @@ $oUserAuthorized = false;
                                             </div>
                                             <figure><img src="/local/images/cheque-papper-bottom.png" alt=""></figure>
                                         </div>
+                                        */ ?>
                                     </div>
                                 </div>
                             </div>
@@ -89,6 +91,79 @@ $oUserAuthorized = false;
                         <a class="button primary" href="/create/">Создать</a>
                     </form>
                 </div>
+            <?php endif; ?>
+        </div>
+    </section>
+    <section class="cashmachine-wrapper">
+        <div class="container">
+            <?php if(!empty($arCashMachines)): ?>
+                <div class="cashmachine-list__inner">
+                    <h1>Ближайшие банкоматы</h1>
+                    <ul class="cashmachine-list">
+                        <?php foreach($arCashMachines as $iKeyCashMachine => $arCashMachine):
+                            if($iColor > 3) $iColor = 1;
+                            ?>
+                            <li class="cashmachine-item">
+                                <figure><img src="/local/images/atm-machines-<?=$iColor?>.jpg" alt="<?=$arCashMachine["ID"]?>"></figure>
+                                <div class="cashmachine-item__inner">
+                                    <div class="cashmachine__item--group">
+                                        <div class="cashmachine__item--group-name">Адрес:</div>
+                                        <div class="cashmachine__item--group-option"><?=$arCashMachine["ADDRESS"]?></div>
+                                    </div>
+                                    <div class="cashmachine__item--group">
+                                        <div class="cashmachine__item--group-name">Режим работы</div>
+                                        <time datetime="" class="cashmachine__item--group-option"><?=$arCashMachine["WORK_TIME"]?></time>
+                                    </div>
+                                </div>
+                            </li>
+                            <?php $iColor++;
+                        endforeach; ?>
+                    </ul>
+                </div>
+                <div class="cashmachine">
+                    <div class="cashmachine__inner blue">
+                        <div class="cashmachine__left-block"></div>
+                        <div class="cashmachine__middle-block">
+                            <div class="cashmachine__middle--top-block">
+                                <div class="cashmachine__middle--top-block-inner">Cash Machine</div>
+                            </div>
+                            <div class="cashmachine__middle--middle-block">
+                                <div class="cashmachine__terminal">
+                                    <div class="cashmachine__first-key cashmachine__key">ok</div>
+                                    <div class="cashmachine__second-key cashmachine__key">отмена</div>
+                                    <div class="cashmachine__third-key cashmachine__key">лимит</div>
+                                    <div class="cashmachine__fourth-key cashmachine__key">снять</div>
+                                    <p class="cashmachine__terminal--text">Введите нужную сумму для снятия и нажмите кнопку
+                                        ok
+                                    </p>
+                                </div>
+                                <div class="cashmachine__middle--middle-block-inner">
+                                    <div class="cashmachine__money-block">
+                                        <p class="cashmachine__money-block--text">CASH</p>
+                                        <div class="cashmachine__money-block--inner"></div>
+                                    </div>
+                                    <ul class="cashmachine__keyboard">
+                                        <?php for($index = 1; $index <= 9; $index++): ?>
+                                            <li class="cashmachine__keyboard-item" data-value="<?=$index?>"><?=$index?></li>
+                                        <?php endfor; ?>
+                                        <li class="cashmachine__keyboard-item cancel">отмена</li>
+                                        <li class="cashmachine__keyboard-item" data-value="0">0</li>
+                                        <li class="cashmachine__keyboard-item ok">ок</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="cashmachine__middle--bottom-block"></div>
+                        </div>
+                        <div class="cashmachine__right-block"></div>
+                    </div>
+                </div>
+            <?php else: ?>
+                <h1 class="form__title">Создайте банкомат</h1>
+                <p class="form__description">Создавать банкоматы может только администратор*</p>
+                <form action="" class="create-form" method="post">
+                    <input type="text" name="phone" hidden>
+                    <a class="button primary" href="/create/">Создать</a>
+                </form>
             <?php endif; ?>
         </div>
     </section>
--- providers/korolev/cashmachine/classes/cashmachine.php
+++ providers/korolev/cashmachine/classes/cashmachine.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace providers\korolev\cashmachine\classes;
-
 class CashMachine
 {
+    const BASE_DIR = __DIR__ . "/../../../../storage/cashmachines/";
+    const LOG_DIR = __DIR__ . "/../../../../log";
     private $arFields;
     private array $arPost;
 
@@ -14,40 +15,49 @@ class CashMachine
         $this->arFields = $this->arPost["FIELDS"];
     }
 
-    public function setCashMachine(int $iLimit = 1000000): void
+    public function setCashMachine(): void
     {
         if(!empty($this->arFields)){
-            $_SESSION["CASH_MACHINE"][session_id()][$this->arFields["NAME"]] = array(
-                "SESSION_ID" => session_id(),
-                "ID" => $this->arFields["NAME"],
-                "ADDRESS" => $this->arFields["ADDRESS"],
-                "WORK_TIME" => $this->arFields["WORK_TIME"],
-                "BANKNOTES" => array(
-                    "5000" => $this->arFields["BANKNOTES"]["5000"],
-                    "2000" => $this->arFields["BANKNOTES"]["2000"],
-                    "1000" => $this->arFields["BANKNOTES"]["1000"],
-                    "500" => $this->arFields["BANKNOTES"]["500"],
-                    "200" => $this->arFields["BANKNOTES"]["200"],
-                    "100" => $this->arFields["BANKNOTES"]["100"],
-                ),
-                "LOAD_CASHMACHINE" => $this->arFields["LOAD_CASHMACHINE"],
-            );
+            $sFilename = self::BASE_DIR . "{$this->arFields["NAME"]}.txt";
+            try{
+                file_put_contents($sFilename,
+                    json_encode(
+                        array(
+                            "ADDRESS" => $this->arFields["ADDRESS"],
+                            "WORK_TIME" => $this->arFields["WORK_TIME"],
+                            "BANKNOTES" => array(
+                                "5000" => $this->arFields["BANKNOTES"]["5000"],
+                                "2000" => $this->arFields["BANKNOTES"]["2000"],
+                                "1000" => $this->arFields["BANKNOTES"]["1000"],
+                                "500" => $this->arFields["BANKNOTES"]["500"],
+                                "200" => $this->arFields["BANKNOTES"]["200"],
+                                "100" => $this->arFields["BANKNOTES"]["100"],
+                            ),
+                            "LOAD_CASHMACHINE" => $this->arFields["LOAD_CASHMACHINE"],
+                        )
+                    )
+                );
+            }catch(\Exception $e){
+                $sFilename = self::LOG_DIR . "cashmachines.txt";
+                file_put_contents($sFilename, PHP_EOL . date("d.m.Y H:i:s") . $e->getMessage(), FILE_APPEND);
+            }
         }
     }
 
-    public function getCashMachine()
+    public function getCashMachine(): array
     {
-        if(isset($_SESSION["CASH_MACHINE"][session_id()])){
-            return $_SESSION["CASH_MACHINE"][session_id()];
-        }else{
-//            $this->setCashMachine();
+        $arResult = array();
+        try{
+            foreach(scandir(self::BASE_DIR) as $file){
+                if(!in_array($file, array(".", ".."))){
+                    $arResult[] = json_decode(file_get_contents(self::BASE_DIR . $file), true);
+                }
+            }
+        }catch(\Exception $e){
+            $sFilename = self::LOG_DIR . "cashmachines.txt";
+            file_put_contents($sFilename, PHP_EOL . date("d.m.Y H:i:s") . $e->getMessage(), FILE_APPEND);
         }
-        return $_SESSION["CASH_MACHINE"][session_id()];
-    }
-
-    public function setLimit($iLimit): void
-    {
-        $this->setCashMachine($iLimit);
+        return $arResult;
     }
 
     public function getBanknotes(): array
